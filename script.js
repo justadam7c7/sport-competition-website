397edbd685e91ebfc9c9ce7c4cd7654b295e771d
justadam7c7 <jusada7c7@gmail.com>
2021-01-20 19:09:38 +0100

    using BEM motodology

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function detectMob() {
   return ((window.innerWidth <= 800) && (window.innerHeight <= 850));
 }
 
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-menu');
-const navLinks = document.querySelectorAll('.nav-links');
+const menu = document.querySelector('#nav__toggle');
+const menuLinks = document.querySelector('.nav__menu');
+const navLinks = document.querySelectorAll('.nav__menu-links');
 
 menu.addEventListener('click', function () {
   menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('nav-menu');
+  menuLinks.classList.toggle('nav__menu');
   menuLinks.classList.toggle('aaa');
 });
 
@@ -16,7 +16,7 @@ for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", function () {
     if (detectMob()) {
       menu.classList.toggle('is-active');
-      menuLinks.classList.toggle('nav-menu');
+      menuLinks.classList.toggle('nav__menu');
       menuLinks.classList.toggle('aaa');
     }
   });
